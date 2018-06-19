3eafbce6681a736c9cf320220a34a819fd6acb47
David William Nartey <nartey.dw@gmail.com>
2018-06-18 22:54:45 -0400

    UIKit Class 2

---
+++ UIKit/UIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIKit
+//
+//  Created by David William Nartey on 6/18/18.
+//  Copyright © 2018 KanDid Consultancy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
